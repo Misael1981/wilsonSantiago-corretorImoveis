7ffed5dffbe9fe85ffc132ad4eeb75ef904f7de9
Misael <misaelborges1981@gmail.com>
2025-10-07 14:05:04 -0300

    Formatações para responsividade

--- components/BannerContactUs/index.jsx
+++ components/BannerContactUs/index.jsx
@@ -11,31 +11,33 @@ const items = [
 
 const BannerContactUs = () => {
   return (
-    <section className="p-4 lg:flex lg:items-stretch">
-      <div className="bg-gradient-wilson-blue flex flex-col justify-between space-y-4 rounded-t-md p-4 lg:flex-2 lg:rounded-l-md lg:rounded-tr-none">
-        <ThirdTitle title="Fale Conosco" />
-        <h4 className="text-xl font-semibold text-white/70">
-          Nós podemos te ajudar a transformar seu sonho em realidade
-        </h4>
-        <ul className="space-y-2">
-          {items.map((item, index) => (
-            <li key={index} className="text-md text-white/70">
-              <CheckCheck className="text-wilson-golden mr-2 inline-block h-5 w-5" />
-              {item}
-            </li>
-          ))}
-        </ul>
-        <Button className="bg-gradient-wilson-golden w-[50%] font-bold text-blue-950">
-          Fale Conosco
-        </Button>
-      </div>
-      <div className="relative h-[350px] lg:w-[400px] lg:flex-1">
-        <Image
-          src="/assets/banner-contato.jpg"
-          alt="banner-contact-us"
-          fill
-          className="rounded-b-md object-cover lg:rounded-r-md lg:rounded-bl-none"
-        />
+    <section className="boxed p-4">
+      <div className="shadow-custom-deep lg:flex lg:items-stretch">
+        <div className="bg-gradient-wilson-blue flex flex-col justify-between space-y-4 rounded-t-md p-4 lg:flex-2 lg:rounded-l-md lg:rounded-tr-none">
+          <ThirdTitle title="Fale Conosco" />
+          <h4 className="text-xl font-semibold text-white/70">
+            Nós podemos te ajudar a transformar seu sonho em realidade
+          </h4>
+          <ul className="space-y-2">
+            {items.map((item, index) => (
+              <li key={index} className="text-md text-white/70">
+                <CheckCheck className="text-wilson-golden mr-2 inline-block h-5 w-5" />
+                {item}
+              </li>
+            ))}
+          </ul>
+          <Button className="bg-gradient-wilson-golden w-[50%] font-bold text-blue-950">
+            Fale Conosco
+          </Button>
+        </div>
+        <div className="relative h-[350px] lg:w-[400px] lg:flex-1">
+          <Image
+            src="/assets/banner-contato.jpg"
+            alt="banner-contact-us"
+            fill
+            className="rounded-b-md object-cover lg:rounded-r-md lg:rounded-bl-none"
+          />
+        </div>
       </div>
     </section>
   )
